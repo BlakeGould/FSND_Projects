b1902d6c676316028f23375ae1447e70072cc3af
Blake Gould <gouldbf@gmail.com>
2021-03-13 11:52:11 -0700

    Implement Venues Search

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -142,14 +142,31 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+  search = request.form.get('search_term', '')
+  venues = Venue.query.filter(Venue.name.ilike("%" + search + "%")).all()
+  response = {
+      "count": len(venues),
+      "data": []
   }
+  for venue in venues:
+      response["data"].append({
+          'id': venue.id,
+          'name': venue.name,
+      })
+  return render_template(
+      'pages/search_venues.html',
+      results=response,
+      search_term=request.form.get('search_term', '')
+  )
+
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 2,
+  #     "name": "The Dueling Pianos Bar",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
